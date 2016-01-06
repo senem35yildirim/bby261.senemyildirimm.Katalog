007f85635705f43fd84c525b6a994335b4dc71d3
senem35yildirim <senem35yildirim@gmail.com>
2016-01-06 13:22:56 +0200

    katalog

---
+++ database.js
@@ -0,0 +1,39 @@
+var catalog = [ 
+ 	{ 
+         "itemName":"Suç ve Ceza", 
+      "itemAuthor":"Fyodor Mihailoviç Dostoyevski", 
+      "itemImage":"images/suc.jpg", 
+         "itemCreationDate":"2015", 
+         "itemDescription":"<p>Eski öğrenci Raskolnikov, kiracıdan kiraladığı daracık odasında beş parasız günler geçirmektedir. Modern zamanların, çağdaş bilimin ve edebiyatın bu yaratıcı, akıllı genci, toplumun gerici bir canavara dönüşmüş karanlık avucunda ezilip un ufak mı olacaktır yoksa? Bir gün oturduğu bir kafede, anarşist düşüncelere sahip gençlerin konuşmalarına kulak misafiri olur ve aklına tüm dertlerine son verecek bir şey gelir. İlk iş olarak da kapıcının kulübesindeki baltayı kestirir gözüne. Dünya edebiyatının en ünlü baltasını...Suç ve Ceza ilk kez 1866 yılında yayımlandı. O tarihten sonra da dünyanın gündeminden hiç düşmedi. Dostoyevski, hiç aceleye gelmemesi gerektiğini düşündüğü yeni bir karakter yakalamıştı. Rusya'yı, Rus halkını gözlemleyerek Raskolnikov'u onların içinden çekip almıştı. Öykü, tüm yönleriyle çürüdüğü açıkça görülen geleneksel iyilik algısının toplum tarafından nasıl öz göre göre korunduğunu anlatır. Dostoyevski'nin Suç ve Cezası, insanlığa sorduğu can alıcı sorularla güncelliğini hiç yitirmeyen en büyük başyapıtlar arasında. Hala...  <br>(Tanıtım Bülteninden)</p>" 
+ },
+    { 
+         "itemName":"Aşk Neden Can Yakar", 
+      "itemAuthor":"Mehmet Yıldız", 
+      "itemImage":"images/ask.jpg", 
+         "itemCreationDate":"2015", 
+         "itemDescription":"<p>Ne yaparsan yap olmaz bazen. Ama o kadar güzel olmaz ki, Ancak bu kadar güzel olmayabilirdi dersin. Ve aklına gelir: Kadere iman eden kederden emin olur. Sonra anlarsın ki, nar tanelerini teker teker yerli yerine yerleştiren Rabbin, seni de hangi gönle yerleştireceğini bilir. Tek yapman gereken kara geceleri kudret kalemiyle güneşe boyayan, kahverengi odundan pembe çiçekler açtıran Allah'a inanmak. O'na inanırsan yaklaşmak için tuttuğun elin aslında Allah'tan uzaklaştırdığını anlarsın. O'na inanırsan batıp gidenlerden medet ummaz kalbin esas sahibine yönelirsin. O'na inanırsan aşk neden can yakar anlar ve sabır ipliğiyle diktiğin tüm yaralarını tedavi edersin. İstemez misin kor ateşler etrafını sararken yanmayan bir İbrahim olmayı...   <br>(Tanıtım Bülteninden)</p>" 
+ },
+    { 
+         "itemName":"Seviyorsan Git Konuş Bence", 
+      "itemAuthor":"Fatih Yağcı", 
+      "itemImage":"images/seviyorsan.jpg", 
+         "itemCreationDate":"2015", 
+         "itemDescription":"<p>Kitap okumayı sevmiyorum ki ben, bunun filmi yok mu? diyecek olanlara, bu hayat filminin yalnız bir kez çekileceğini ve aslında bu filmin başrol oyuncusunun kendisi olduğunu hatırlatan bu kitabı, önce kendinize, daha sonra bittiği gibi sevdiğiniz bütün namaz kılmakta zorlanan arkadaşlarınıza vermeye can attığınızı daha ilk sayfalarda hissedeceksiniz. Sempatik ve sıra dışı anlatımıyla, gülerken duygulandığın birkaç saat... Âlemlerin Rabbine olan samimiyet ve aşkının şiddetini arttıracak birkaç ipucu... Manevi atmosferinin güçlenerek ibadetlerinde huşu ve huzuru yakalamana vesile birkaç satır...  <br>(Tanıtım Bülteninden)</p>" 
+ },
+    { 
+         "itemName":"İçiyorsak Sebebi Var", 
+      "itemAuthor":"Harun Serkan Aktaş", 
+      "itemImage":"images/iciyorsak.jpg", 
+         "itemCreationDate":"2015", 
+         "itemDescription":"<p>Derdin hep yanında ise şükret!Derdin derman olduğu gibi aynı zamanda dosttur sana…Ve hep acıları söyler.Gecenin en demli anında gelir, çayıma ortak olur. Ve sorar: Nedendir gece gece bu kadar çay?Çayımdan bir yudum alıp masaya bırakmamdan alır cevabını:- İçiyorsak sebebi var dost…Eminim kitabı okuyan her genç Benim de bir derdim var ve içiyorsam sebebi var diyecek.O dert ki, bir davanın kimliği ve ebedi hayatın anahtarı…Satır aralarında ilerledikçe iman gözlüğünü takıp kâinatı seyretmenin huzuruna varacak…Ve Kur'an eczanesinden bu asrın manevi hastalıklarına şifalar sunan 'kırmızı kitaplar'a aşkını tazeleyecek. <br>(Tanıtım Bülteninden)</p>" 
+ },
+     { 
+         "itemName":"Benim Olağanüstü Akıllı Arkadaşım", 
+      "itemAuthor":"Elena Ferrante", 
+      "itemImage":"images/benim.jpg", 
+         "itemCreationDate":"2015", 
+         "itemDescription":"<p>Kimliğini ısrarla gizlemesiyle adeta bir efsane haline gelen müstear isimli Elena Ferrante'nin dört ciltten oluşan Napoli Romanları, sadece İtalya'da değil tüm dünyada fenomen olarak kısa sürede 22 dile çevrildi. Bugün milyonlarca okurun hayranlıkla okuduğu dizinin devam kitaplarının da en kısa sürede raflardaki yerini alacağını belirtiriz.Sen benim olağanüstü akıllı arkadaşımsın, hepimizden çok daha başarılı olmalısın, bütün kızlardan ve erkeklerden.Benim Olağanüstü Akıllı Arkadaşım, İtalya'da bir kenar mahallede yetişen iki genç kızın çekişmeler, kıskançlıklar ve sırlarla örülü dostluklarını, zorluklarla geçen büyüme ve varoluş serüvenlerini anlatıyor.Napoli Romanlarının ilki 50'lerde, fakir bir mahallede başlıyor. Bu unutulmaz dostluk hikâyesinde fazlasıyla akıllı ve duyarlı iki genç kız, Lenù ile Lila, boğucu erkek-egemen kültür, duyarsız, buyurgan aileleri ve yoksunluklar karşısında birbirlerinde teselli bulur. Ancak bu iki sıradışı arkadaş büyüdükçe, onlara dayatılan değerleri kabule yanaşmayacak, büyük fedakârlıklar da gerektirse, birer kadın olarak tutkularını yaşamak ve yaratıcı olmak için ellerinden geleni yapacaktır… <br>(Tanıtım Bülteninden)</p>" 
+ }
+   
+
+ ]; 
